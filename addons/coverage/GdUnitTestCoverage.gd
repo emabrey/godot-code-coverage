b67f87096e4d42e055b189707b35dd0ed6a83cea
Emily Mabrey <emilymabrey93@gmail.com>
2025-02-12 05:07:11 -0500

    Refactor code coverage class
    Signed-off-by: Emily Mabrey <emilymabrey93@gmail.com>

--- addons/coverage/GdUnitTestCoverage.gd
+++ addons/coverage/GdUnitTestCoverage.gd
@@ -1,10 +1,10 @@
 class_name GdUnitTestCoverage
 extends RefCounted
 
-# Minimum overall coverage target
+# Minimum default overall coverage target required for a successful code coverage check
 static var DEFAULT_COVERAGE_TARGET := 0.0
 
-# Minimum per file coverage target
+# Minimum default per file coverage target required for a successful code coverage check
 static var DEFAULT_FILE_TARGET := 0.0
 
 # Default paths to exclude from coverage instrumentation
@@ -14,22 +14,41 @@ static var DEFAULT_EXCLUDE_PATHS = [
 	"res://tests/*",
 ]
 
-static var instance
+static var instance : TestCoverage
 
-static func setup_test_coverage(tree : SceneTree, exclude_paths = DEFAULT_EXCLUDE_PATHS):
+## Setup the test coverage checker. Uses the given tree for instantiating the TestCoverage
+## node. The specified excluded paths will not be instrumented and checked for test coverage.
+## It is very important that the test coverage exlcude the test classes themselves within
+## the excluded paths, as otherwise a circular instrumentation of instrumented classes
+## will result.
+static func setup(tree : SceneTree, exclude_paths = DEFAULT_EXCLUDE_PATHS):
 	assert(instance == null, "Do not overwrite class singleton instance by calling setup twice")
 	assert(tree != null, "The scene tree for initializing test coverage class may not be null")
 	instance = TestCoverage.new(tree, exclude_paths)
 
+## Optionally generates a report file at the specified filename location; defaults to using
+## the value of env var COVERAGE_FILE if no other value is specified.
 static func set_report_file(filename := OS.get_environment("COVERAGE_FILE")):
-	assert(instance != null, "Before using this method ensure you setup coverage with setup_test_coverage")
+	assert(instance != null, "Before using this method ensure you setup coverage with setup function")
 	instance.save_coverage_file(filename)
 
+## Add a directory whose child scripts should be includeded and checked for test coverage.
 static func add_checked_directory(source_dir : String):
-	assert(instance != null, "Before using this method ensure you setup coverage with setup_test_coverage")
+	assert(instance != null, "Before using this method ensure you setup coverage with setup function")
 	instance.instrument_scripts(source_dir)
 
-static func teardown_test_coverage(verbosity := TestCoverage.Verbosity.FILENAMES) -> bool:
-	assert(instance != null, "Before using this method ensure you setup coverage with setup_test_coverage")
+## Configure what the acceptable minimum code coverage is for the overall set of scripts
+## instrumented, as well as the minimum code coverage percentage required of each individual
+## script file. Defaults to a target of 0% for both, which always successfully validates
+## that the code coverage tagets were met.
+static func set_targets(per_file_target := DEFAULT_FILE_TARGET, overall_target := DEFAULT_COVERAGE_TARGET):
+	assert(per_file_target >= 0.0 && per_file_target <= 100.0, "Invalid per file target percentage given")
+	assert(overall_target >= 0.0 && overall_target <= 100.0, "Invalid overall target percentage given")
+	instance.set_coverage_targets(per_file_target, overall_target)
+
+## Once finished adding all the instrumented code, this method finalizes the code coverage
+## analysis. Returns true if the code coverage passsed with the
+static func finish(verbosity := TestCoverage.Verbosity.FILENAMES) -> bool:
+	assert(instance != null, "Before using this method ensure you setup coverage with setup function")
 	instance._finalize(verbosity)
 	return  instance.coverage_passing()
